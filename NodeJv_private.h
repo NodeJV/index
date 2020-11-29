68b43337d5caf38037b253710320e48813dc7608
The Artificial Intelligence <64889395+ChengT-cmd@users.noreply.github.com>
2020-11-29 13:29:58 +0800

    Add files via upload

---
+++ NodeJv_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef NODEJV_PRIVATE_H
+#define NODEJV_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"2.4.385.453730"
+#define VER_MAJOR	2
+#define VER_MINOR	4
+#define VER_RELEASE	385
+#define VER_BUILD	453730
+#define COMPANY_NAME	"2020 (c) Jvav Studios"
+#define FILE_VERSION	"2.4.385.453730"
+#define FILE_DESCRIPTION	"NodeJv"
+#define INTERNAL_NAME	"NodeJv.exe"
+#define LEGAL_COPYRIGHT	"Jvav Studios"
+#define LEGAL_TRADEMARKS	"Jvsv Studios"
+#define ORIGINAL_FILENAME	"NodeJv.exe"
+#define PRODUCT_NAME	"NodeJv.exe"
+#define PRODUCT_VERSION	"2.4.385.453730"
+
+#endif /*NODEJV_PRIVATE_H*/
